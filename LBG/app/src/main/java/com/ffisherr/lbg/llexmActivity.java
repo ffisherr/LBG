eeece4ab51b22bd711a0b372385db6010c64dcd7
kudryavtsev <lexus1450@gmail.com>
2020-03-07 13:07:00 +0300

    llexm joined

---
+++ LBG/app/src/main/java/com/ffisherr/lbg/llexmActivity.java
@@ -0,0 +1,14 @@
+package com.ffisherr.lbg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class llexmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llexm);
+    }
+}
