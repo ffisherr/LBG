16122fb8c62cc92e441c68002b06e05fec1179a8
kudryavtsev <lexus1450@gmail.com>
2020-03-10 11:55:15 +0300

    создание элекментов из кода

--- LBG/app/src/main/java/com/ffisherr/lbg/llexmActivity.java
+++ LBG/app/src/main/java/com/ffisherr/lbg/llexmActivity.java
@@ -1,10 +1,16 @@
 package com.ffisherr.lbg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class llexmActivity extends AppCompatActivity {
+    LinearLayout calendar_layout = (LinearLayout) findViewById(R.id.calendar_layout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,21 @@ public class llexmActivity extends AppCompatActivity {
 
     protected void serverAnswered() {
         //TODO добавить все пришедшие события в два layout'а, которые будут отображаться попеременно
+
+        //TODO в цикле перебора пришедших событий они добавляются в два layouta, из которых отображается один
+        int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+        LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                0, wrapContent);
+        lParams.gravity = Gravity.LEFT;
+        LinearLayout newLayout = new LinearLayout(calendar_layout.getContext());
+        newLayout.setOrientation(LinearLayout.VERTICAL);
+        calendar_layout.addView(newLayout, lParams);
+        TextView newLabel = new TextView(newLayout.getContext());
+        newLabel.setText("текст события");
+        newLayout.addView(newLabel);
+        Button newButton = new Button(newLayout.getContext());
+        newButton.setText("+");
+        newLayout.addView(newButton);
+
     }
 }
